f6958bee24f75052301c9956eb8fac2955feb606
DC <szdcboy@gmail.com>
2020-06-30 11:35:00 +0800

    fix: index.js

--- index.js
+++ index.js
@@ -1,26 +1,107 @@
-import React, { cloneElement, PropTypes } from 'react';
-import { WebView, NativeModules, requireNativeComponent } from 'react-native';
+import React, { cloneElement } from 'react';
+import { WebView, UIManager, requireNativeComponent ,NativeModules} from 'react-native';
+import PropTypes from 'prop-types';
 
-var RCT_WEBVIEW_REF = 'webview';
+export default class extends WebView {
 
-class X5WebView extends WebView {
+    static displayName = 'X5WebView';
+
+    static propTypes = {
+        ...WebView.propTypes,
+        // keyboardDisplayRequiresUserAction: PropTypes.bool,
+        // allowFileAccessFromFileURLs: PropTypes.bool,
+        // hideAccessory: PropTypes.bool,
+        // webviewDebugEnabledWhenDev: PropTypes.number
+    };
+
     static getX5CoreVersion = function (cb: Function): Promise {
-        if (cb) {
-            NativeModules.X5WebView.getX5CoreVersion(cb);
-        }
+      if (cb) {
+          NativeModules.RNX5WebView.getX5CoreVersion(cb);
+      }
 
-        return new Promise(resolve => {
-            NativeModules.X5WebView.getX5CoreVersion(resolve);
+      return new Promise(resolve => {
+          NativeModules.RNX5WebView.getX5CoreVersion(resolve);
+      });
+    };
+
+    goForward = () => {
+        UIManager.dispatchViewManagerCommand(
+            this.getWebViewHandle(),
+            UIManager.getViewManagerConfig('RNX5WebView').Commands.goForward,
+            null
+        );
+    };
+
+    goBack = () => {
+        UIManager.dispatchViewManagerCommand(
+            this.getWebViewHandle(),
+            UIManager.getViewManagerConfig('RNX5WebView').Commands.goBack,
+            null
+        );
+    };
+
+    reload = () => {
+        UIManager.dispatchViewManagerCommand(
+            this.getWebViewHandle(),
+            UIManager.getViewManagerConfig('RNX5WebView').Commands.reload,
+            null
+        );
+    };
+
+    stopLoading = () => {
+        UIManager.dispatchViewManagerCommand(
+            this.getWebViewHandle(),
+            UIManager.getViewManagerConfig('RNX5WebView').Commands.stopLoading,
+            null
+        );
+    };
+
+    postMessage = (data) => {
+        UIManager.dispatchViewManagerCommand(
+            this.getWebViewHandle(),
+            UIManager.getViewManagerConfig('RNX5WebView').Commands.postMessage,
+            [String(data)]
+        );
+    };
+
+    injectJavaScript = (data) => {
+        UIManager.dispatchViewManagerCommand(
+            this.getWebViewHandle(),
+            UIManager.getViewManagerConfig('RNX5WebView').Commands.injectJavaScript,
+            [data]
+        );
+    };
+
+    _onLoadingError = (event) => {
+        event.persist(); // persist this event because we need to store it
+        var {onError, onLoadEnd} = this.props;
+        var result = onError && onError(event);
+        onLoadEnd && onLoadEnd(event);
+        console.warn('Encountered an error loading page', event.nativeEvent);
+
+        result !== false && this.setState({
+            lastErrorEvent: event.nativeEvent,
+            viewState: 'ERROR'
         });
+    };
+
+    onLoadingError = (event) => {
+        this._onLoadingError(event)
     };
 
     render() {
         const wrapper = super.render();
         const [webview,...children] = wrapper.props.children;
+        // const { hideAccessory, allowFileAccessFromFileURLs, keyboardDisplayRequiresUserAction,webviewDebugEnabledWhenDev} = this.props;
+
         const X5webview = (
             <RNX5WebView
-                ref={ RCT_WEBVIEW_REF }
                 {...webview.props}
+                ref="webview"
+                // allowFileAccessFromFileURLs={allowFileAccessFromFileURLs}
+                // keyboardDisplayRequiresUserAction={keyboardDisplayRequiresUserAction}
+                // hideAccessory={hideAccessory}
+                // webviewDebugEnabledWhenDev={webviewDebugEnabledWhenDev}
             />
         );
 
@@ -28,10 +109,10 @@ class X5WebView extends WebView {
     }
 }
 
-const RNX5WebView = requireNativeComponent('RNX5WebView', X5WebView, {
+const RNX5WebView = requireNativeComponent('RNX5WebView', null, {
     nativeOnly: {
-        messagingEnabled: PropTypes.bool,
-    },
-});
-
-export default X5WebView;
+        messagingEnabled:true,
+        allowFileAccessFromFileURLs: true,
+        hideAccessory: true,
+    }
+})
